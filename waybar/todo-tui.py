6dd982aacdd8d123ff9600c88b88b5ae536b6d92
Alex Jercan <jercan_alex27@yahoo.com>
2023-10-19 15:42:55 +0300

    fix: better wrapper

--- waybar/todo-tui.py
+++ waybar/todo-tui.py
@@ -4,26 +4,30 @@ import html
 import json
 import subprocess
 
-status = subprocess.run(
-    ["todo-tui", "status"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-    text=True,
-    check=True
-)
-details = subprocess.run(
-    ["todo-tui", "details"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-    text=True,
-    check=True
-)
-
-data = {}
-
-data['text'] = status.stdout.strip()
-data['tooltip'] = html.escape(details.stdout.strip())
+try:
+    status = subprocess.run(
+        ["todo-tui", "status"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        text=True,
+        check=True
+    )
+    details = subprocess.run(
+        ["todo-tui", "details"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        text=True,
+        check=True
+    )
+except Exception as e:
+    data = {}
+    data['text'] = "N/A"
+    data['tooltip'] = str(e)
+else:
+    data = {}
+    data['text'] = status.stdout.strip()
+    data['tooltip'] = html.escape(details.stdout.strip())
 
 print(json.dumps(data))
